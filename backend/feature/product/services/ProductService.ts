c737119ff065aba603779d3ab7f0312d4a345c78
youngbeen <pybju0@gmail.com>
2025-01-31 18:36:28 +0900

    Comment 주석 해제

--- backend/feature/product/services/ProductService.ts
+++ backend/feature/product/services/ProductService.ts
@@ -1,14 +1,14 @@
 import pool from "../../../config/dbConfig";
 
 interface Product {
-  // product_code: string;
+  product_code: string;
   category_id: number;
   product_name: string;
   description: string;
   origin_price: number;
-  // discount_price: number; 
+  discount_price: number; 
   final_price: number;
-  // stock_quantity: number;
+  stock_quantity: number;
   product_status: string;
   sizes: string[];
   colors: string[];
@@ -19,14 +19,14 @@ class ProductService {
   static async createProduct(product: Product) {
     try {
       const {
-        // product_code,
+        product_code,
         category_id,
         product_name,
         description,
         origin_price,
-        // discount_price,
+        discount_price,
         final_price,
-        // stock_quantity,
+        stock_quantity,
         product_status,
         sizes,
         colors,
@@ -39,14 +39,14 @@ class ProductService {
       `;
 
       const values = [
-        // product_code,
+        product_code,
         category_id,
         product_name,
         description,
         origin_price,
-        // discount_price,
+        discount_price,
         final_price,
-        // stock_quantity,
+        stock_quantity,
         product_status,
         JSON.stringify(sizes),
         JSON.stringify(colors),
